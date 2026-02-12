6163e20b3bf47afe8fd7d874910e47039fc07c97
itsmevichu <vishnutheep@gmail.com>
2026-02-13 03:55:45 +0530

    fix docker compose

--- module/spring-boot-mongodb/src/main/java/org/springframework/boot/mongodb/docker/compose/MongoDockerComposeConnectionDetailsFactory.java
+++ module/spring-boot-mongodb/src/main/java/org/springframework/boot/mongodb/docker/compose/MongoDockerComposeConnectionDetailsFactory.java
@@ -17,11 +17,12 @@
 package org.springframework.boot.mongodb.docker.compose;
 
 import com.mongodb.ConnectionString;
-
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.docker.compose.core.RunningService;
 import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionDetailsFactory;
 import org.springframework.boot.docker.compose.service.connection.DockerComposeConnectionSource;
 import org.springframework.boot.mongodb.autoconfigure.MongoConnectionDetails;
+import org.springframework.boot.ssl.SslBundle;
 
 /**
  * {@link DockerComposeConnectionDetailsFactory} to create {@link MongoConnectionDetails}
@@ -54,8 +55,11 @@ class MongoDockerComposeConnectionDetailsFactory extends DockerComposeConnection
 
 		private final ConnectionString connectionString;
 
+		private final @Nullable SslBundle sslBundle;
+
 		MongoDockerComposeConnectionDetails(RunningService service) {
 			super(service);
+			this.sslBundle = getSslBundle(service);
 			this.connectionString = buildConnectionString(service);
 
 		}
@@ -78,6 +82,11 @@ class MongoDockerComposeConnectionDetailsFactory extends DockerComposeConnection
 				builder.append("?authSource=admin");
 			}
 			return new ConnectionString(builder.toString());
+		}
+
+		@Override
+		public @Nullable SslBundle getSslBundle() {
+			return this.sslBundle;
 		}
 
 		@Override
